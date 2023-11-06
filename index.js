7a01f78250360e914da289b803b015f3a25b6cfc
Joey Laya <joeyazlaya@gmail.com>
2023-11-06 14:29:26 -0800

    build(deps): update index.js, package-lock.json and package.json

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from 'express'
 import multer from 'multer'
+import cors from 'cors'
 
 const upload = multer({ dest: 'uploads/' })
 
 const app = express()
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html')
 })
 
 app.post('/api/metadata', upload.single('upfile'), (req, res) => {
-  console.log(req.file)
   res.status(200).json({
     name: req.file.originalname,
     type: req.file.mimetype,
